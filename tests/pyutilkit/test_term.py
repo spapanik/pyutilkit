a6e3a67f7321b49da22b8e83f57e9cd79535ef50
Stephanos Kuma <stephanos@kuma.ai>
2024-12-05 16:23:54 +0000

    🔬 Add more tests for SGRString

--- tests/pyutilkit/test_term.py
+++ tests/pyutilkit/test_term.py
@@ -1,3 +1,6 @@
+import sys
+from unittest import mock
+
 import pytest
 
 from pyutilkit.term import SGRCodes, SGRString
@@ -19,6 +22,12 @@ def test_sgr_string_with_default() -> None:
     assert isinstance(sgr_string, str)
 
 
+def test_sgr_string_length() -> None:
+    string = "Hello, World"
+    sgr_string = SGRString(string, prefix="¡", suffix="!")
+    assert len(sgr_string) == 14
+
+
 def test_sgr_string_set_attribute() -> None:
     string = "Hello, World!"
     sgr_string = SGRString(string, params=[SGRCodes.BOLD, SGRCodes.RED])
@@ -32,3 +41,44 @@ def test_sgr_string_del_attribute() -> None:
 
     with pytest.raises(AttributeError):
         del sgr_string._sgr  # noqa: SLF001
+
+
+def test_sgr_string_multiplication() -> None:
+    sgr_string = SGRString(
+        "*", params=[SGRCodes.BOLD, SGRCodes.RED], prefix="x", suffix="x"
+    )
+    sgr_string_mul = sgr_string * 3
+    assert str(sgr_string_mul) == "x\x1b[1m\x1b[31m***\x1b[0mx"
+
+
+def test_sgr_string_right_multiplication() -> None:
+    sgr_string = SGRString(
+        "*", params=[SGRCodes.BOLD, SGRCodes.RED], prefix="x", suffix="x"
+    )
+    sgr_string_mul = sgr_string * 3
+    sgr_string_rmul = 3 * sgr_string
+    assert str(sgr_string_rmul) == str(sgr_string_mul)
+
+
+@mock.patch("pyutilkit.term.print", new_callable=mock.MagicMock(spec=print))
+@mock.patch("pyutilkit.term.sys.stdout", new=mock.MagicMock(spec=sys.stdout))
+def test_sgr_string_print(mock_print: mock.MagicMock) -> None:
+    sgr_string = SGRString(
+        "*", params=[SGRCodes.BOLD, SGRCodes.RED], prefix="x", suffix="x"
+    )
+    sgr_string.print()
+    assert mock_print.call_count == 1
+
+    calls = [
+        mock.call(
+            "x",
+            "\x1b[1m\x1b[31m",
+            "*",
+            "\x1b[0m",
+            "x",
+            sep="",
+            end="\n",
+            file=sys.stdout,
+        )
+    ]
+    assert mock_print.call_args_list == calls
